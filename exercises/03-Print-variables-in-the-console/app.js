9c241722d15c32edd9367bf11004718543d45a73
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-09 16:26:24 -0400

    exercise 3

--- exercises/03-Print-variables-in-the-console/app.js
+++ exercises/03-Print-variables-in-the-console/app.js
@@ -1,4 +1,9 @@
-var mySuperVariable = 'hello';
-console.log(mySuperVariable);
+//declaring the array
+var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
 
-// Your code below:
+//1. print the item here
+console.log(myArray[2])
+//2. change 'thursday'a value here to null
+myArray.splice(4,1,null)
+//3. print the position of step 2
+console.log(myArray[4])
