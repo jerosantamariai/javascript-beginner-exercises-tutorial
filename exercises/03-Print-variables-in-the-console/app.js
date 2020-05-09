f700ec69dd50cb1d3e95c0f3bf43057d9207fdd2
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-09 16:39:05 -0400

    redo exercise 3 and add solution of exercise 4

--- exercises/03-Print-variables-in-the-console/app.js
+++ exercises/03-Print-variables-in-the-console/app.js
@@ -1,9 +1,7 @@
-//declaring the array
-var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+var mySuperVariable = 'hello';
+console.log(mySuperVariable);
 
-//1. print the item here
-console.log(myArray[2])
-//2. change 'thursday'a value here to null
-myArray.splice(4,1,null)
-//3. print the position of step 2
-console.log(myArray[4])
+// Your code below:
+
+let color = "red";
+console.log(color);
